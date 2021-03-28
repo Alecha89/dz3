b7c7b92af98f7bd60d4d8f07abbf109f1b3c27ce
Alecha89 <Alecha89@yandex.ru>
2021-03-28 23:19:24 +0300

    dz3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,80 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
+//-----------------------------------------------------------------------------------------------
+//        int[] month = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+//        for (int i = 0; i < month.length; i++) {
+//            if(month[i] >= 3 & month[i] < 6) {System.out.println("Весна");}
+//            else if(month[i] >= 6 & month[i] < 9) {System.out.println("Лето");}
+//            else if(month[i] >= 9 & month[i] < 12) {System.out.println("Осень");}
+//            else {System.out.println("Зима");}
+//        }
+//-----------------------------------------------------------------------------------------------
+//     int[] month = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+//        for (int i = 0; i < month.length; i++) {
+//          switch (month[i]) {
+//              case 1:
+//                  System.out.println("Зима");
+//                  break;
+//              case 2:
+//                  System.out.println("Зима");
+//                  break;
+//              case 3:
+//                  System.out.println("Весна");
+//                  break;
+//              case 4:
+//                  System.out.println("Весна");
+//                  break;
+//              case 5:
+//                  System.out.println("Весна");
+//                  break;
+//              case 6:
+//                  System.out.println("Лето");
+//                  break;
+//              case 7:
+//                  System.out.println("Лето");
+//                  break;
+//              case 8:
+//                  System.out.println("Лето");
+//                  break;
+//              case 9:
+//                  System.out.println("Осень");
+//                  break;
+//              case 10:
+//                  System.out.println("Осень");
+//                  break;
+//              case 11:
+//                  System.out.println("Осень");
+//                  break;
+//              case 12:
+//                  System.out.println("Зима");
+//                  break;
+//              default:
+//                  System.out.println("Неизвестное число");
+//          }
+//        }
+//-----------------------------------------------------------------------------------------------
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = (array.length-1); i != 0; i--) {
+//           System.out.println(array[i]);
+//        }
+//-----------------------------------------------------------------------------------------------
+//        int[][] squareArray = new int[2][3];
+//        squareArray[0][0] = 1;
+//        squareArray[0][1] = 2;
+//        squareArray[0][2] = 3;
+//        squareArray[1][0] = 4;
+//        squareArray[1][1] = 5;
+//        squareArray[1][2] = 6;
+//
+//        for (int i = 0; i < squareArray.length; i++) {
+//            for (int j = 0; j < squareArray[i].length; j++) {
+//                System.out.print(squareArray[i][j] + "["+i+"]"+"["+j+"] ");
+//            }
+//            System.out.println();
+//        }
+//-----------------------------------------------------------------------------------------------
 
     }
 }
+
